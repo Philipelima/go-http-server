737f11865b977d7b03eae6c9026d008d750f7451
Philipelima <philipematheus.lim@gmail.com>
2022-11-20 19:39:06 -0300

    feat: package main

---
+++ cmd/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/philipelima/http-server/server"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != "GET" {
+		http.Error(w, "Not Allowed", http.StatusMethodNotAllowed)
+	}
+
+	fmt.Fprintf(w, "<h1>Hello World</h1>")
+}
+
+
+
+func main() {
+
+	server := server.Server{}
+
+	server.SetDirectory("./static")
+	server.SetSettings("127.0.0.1", "4000")
+
+	server.Get("/hello-world", hello)
+
+	server.Run()
+
+}
